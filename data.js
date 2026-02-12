ba0dfb558415e033c0e946f2c0b152bcc69a2038
josebowen <jazzbowen@gmail.com>
2026-02-12 14:48:48 -0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,114 @@
+const composers = [
+  {
+    "name": "Ernest Bloch",
+    "birth": 1880,
+    "death": 1959,
+    "country": "Switzerland",
+    "imslp": "https://imslp.org/wiki/Category:Bloch,_Ernest",
+    "works": [
+      {
+        "title": "Baal Shem (Suite h\u00e9bra\u00efque)",
+        "year": 1923,
+        "type": "Piano transcription"
+      }
+    ]
+  },
+  {
+    "name": "Darius Milhaud",
+    "birth": 1892,
+    "death": 1974,
+    "country": "France",
+    "imslp": "https://imslp.org/wiki/Category:Milhaud,_Darius",
+    "works": [
+      {
+        "title": "Saudades do Brasil, Op.67",
+        "year": 1920,
+        "type": "Solo piano"
+      }
+    ]
+  },
+  {
+    "name": "Felix Mendelssohn",
+    "birth": 1809,
+    "death": 1847,
+    "country": "Germany",
+    "imslp": "https://imslp.org/wiki/Category:Mendelssohn,_Felix",
+    "works": [
+      {
+        "title": "Lieder ohne Worte",
+        "year": 1829,
+        "type": "Solo piano"
+      }
+    ]
+  },
+  {
+    "name": "Fanny Mendelssohn Hensel",
+    "birth": 1805,
+    "death": 1847,
+    "country": "Germany",
+    "imslp": "https://imslp.org/wiki/Category:Hensel,_Fanny",
+    "works": [
+      {
+        "title": "Das Jahr",
+        "year": 1841,
+        "type": "Solo piano cycle"
+      }
+    ]
+  },
+  {
+    "name": "Alexander Krein",
+    "birth": 1883,
+    "death": 1951,
+    "country": "Russia",
+    "imslp": "https://imslp.org/wiki/Category:Krein,_Alexander",
+    "works": [
+      {
+        "title": "Hebrew Sketches",
+        "year": 1910,
+        "type": "Piano"
+      }
+    ]
+  },
+  {
+    "name": "Joseph Achron",
+    "birth": 1886,
+    "death": 1943,
+    "country": "Lithuania",
+    "imslp": "https://imslp.org/wiki/Category:Achron,_Joseph",
+    "works": [
+      {
+        "title": "Hebrew Melody (piano version)",
+        "year": 1911,
+        "type": "Piano transcription"
+      }
+    ]
+  },
+  {
+    "name": "Viktor Ullmann",
+    "birth": 1898,
+    "death": 1944,
+    "country": "Czech Republic",
+    "imslp": "https://imslp.org/wiki/Category:Ullmann,_Viktor",
+    "works": [
+      {
+        "title": "Piano Sonata No. 6",
+        "year": 1943,
+        "type": "Solo piano"
+      }
+    ]
+  },
+  {
+    "name": "Erik \u00c5kerberg",
+    "birth": 1860,
+    "death": 1938,
+    "country": "Sweden",
+    "imslp": "https://imslp.org/wiki/Category:\u00c5kerberg,_Erik",
+    "works": [
+      {
+        "title": "Hebrew Melodies",
+        "year": "",
+        "type": "Piano"
+      }
+    ]
+  }
+];
